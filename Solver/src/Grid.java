71d98e6a3d3682621ba6de071ac1c82bd74d06da
wagblin3 <wagblin3@gmail.com>
2022-10-24 13:40:22 +0200

    commit idea

---
+++ Solver/src/Grid.java
@@ -0,0 +1,41 @@
+public class Grid {
+    int[][] arGlobToSolve= new int[9][9];
+    int[][] arUnit = new int[3][3];
+
+    public int[][] getArGlobToSolve() {
+        return arGlobToSolve;
+    }
+
+    public void setArGlobToSolve(int[][] arGlobToSolve) {
+        this.arGlobToSolve = arGlobToSolve;
+    }
+
+    public int[][] getArUnit() {
+        return arUnit;
+    }
+
+    public void setArUnit(int[][] arUnit) {
+        this.arUnit = arUnit;
+    }
+
+
+
+    
+    public void load() {
+
+    }
+
+    public void print() {
+
+    }
+
+    public void solve() {
+        
+
+    }
+
+    public void save() {
+
+    }
+
+}
